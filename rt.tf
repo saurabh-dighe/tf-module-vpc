5b840b9c358cbde96ecd0be21a298ae2cd161e1d
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-03 23:49:33 +0530

    auto push

--- rt.tf
+++ rt.tf
@@ -11,6 +11,12 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
+#aws_public_subnet_route_table_association
+resource "aws_route_table_association" "public_rt_association" {
+  count             = length(aws_subnet.public_subnet.*.id)
+  subnet_id         = element(aws_subnet.public_subnet.*.id, count.index)
+  route_table_id    = aws_route_table.public_rt.id
+}
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.roboshop_dev.id
 
@@ -22,4 +28,10 @@ resource "aws_route_table" "private_rt" {
   tags = {
     Name = "roboshop-${var.ENV}-private_rt"
   }
+}
+
+resource "aws_route_table_association" "private_rt_association" {
+  count             = length(aws_subnet.private_subnet.*.id)
+  subnet_id         = element(aws_subnet.private_subnet.*.id, count.index)
+  route_table_id    = aws_route_table.private_rt.id
 }
