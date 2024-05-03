4e4579e37b25d366e489362d3489dad14d61cb76
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-03 23:36:09 +0530

    auto push

---
+++ rt.tf
@@ -0,0 +1,25 @@
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.roboshop_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-public_rt"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.roboshop_vpc.id
+
+#   route {
+#     cidr_block = var.VPC_CIDR
+#     gateway_id = aws_internet_gateway.ngw.id
+#   }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-private_rt"
+  }
+}
