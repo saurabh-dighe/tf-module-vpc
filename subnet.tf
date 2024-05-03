94573718174b98d2c29e56dd6af545a80ab7ad2a
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-03 13:55:16 +0530

    auto push

--- subnet.tf
+++ subnet.tf
@@ -1,9 +1,21 @@
-resource "aws_subnet" "roboshop-public-subnet-1" {
-  count      = length(var.PRIVATE_SUBNET_CIDR)
+#Provisioning public subnet
+resource "aws_subnet" "public_subnet" {
+  count      = length(var.PUBLIC_SUBNET_CIDR)
   vpc_id     = aws_vpc.roboshop-dev.id
-  cidr_block = [var.PUBLIC_SUBNET_CIDR]
+  cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
 
   tags = {
-    Name = "roboshop-public-subnet-1"
+    Name = "public_subnet-$(var.ENV)-$(count.index)"
+  }
+}
+
+#Provisioning private subnet
+resource "aws_subnet" "private_subnet" {
+  count      = length(var.PRIVATE_SUBNET_CIDR)
+  vpc_id     = aws_vpc.roboshop-dev.id
+  cidr_block = element(var.PRIVATE_SUBNET_CIDR, count.index)
+
+  tags = {
+    Name = "private_subnet-$(var.ENV)-$(count.index)"
   }
 }
