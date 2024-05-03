a24fe77e57826ba57ba8357fa654e2a515943ff9
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-04 01:05:09 +0530

    auto push

--- peering.tf
+++ peering.tf
@@ -1,7 +1,6 @@
 #Provioning VPC peering
 
 resource "aws_vpc_peering_connection" "peering_connection" {
-  peer_owner_id = "996658305414"
   peer_vpc_id   = aws_vpc.DEFAULT_VPC.id
   vpc_id        = aws_vpc.roboshop_dev.id
   auto_accept   = true
@@ -9,14 +8,6 @@ resource "aws_vpc_peering_connection" "peering_connection" {
   tags = {
     Name = "roboshop-${var.ENV}-peering_connection"
   }
-}
-
-resource "aws_vpc" "foo" {
-  cidr_block = var.VPC_CIDR
-}
-
-resource "aws_vpc" "bar" {
-  cidr_block = var.DEFAULT_VPC_CIDR
 }
 
 data "aws_vpc" "DEFAULT_VPC" {
