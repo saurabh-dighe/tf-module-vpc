141b2ef366cc1913af211f6ababab7146022583a
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-03 13:02:23 +0530

    auto push

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc" "roboshop-dev" {
-  cidr_block       = var.VPC_CIDR
-  instance_tenancy = "default"
+  cidr_block            = var.VPC_CIDR
+  instance_tenancy      = "default"
+  enable_dns_hostnames  = true
 
   tags = {
     Name = "roboshop-${var.ENV}-vpc"
