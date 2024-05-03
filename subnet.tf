0e52f3d235d01a4d553fc62c42c173a10af03205
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-03 15:03:40 +0530

    auto push

--- subnet.tf
+++ subnet.tf
@@ -1,10 +1,10 @@
 #Provisioning public subnet
 resource "aws_subnet" "public_subnet" {
-  count      = length(var.PUBLIC_SUBNET_CIDR)
-  vpc_id     = aws_vpc.roboshop-dev.id
-  cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
-  availability_zone = element(var.ZONE, count.index)
-  map_public_ip_on_launch = true
+  count                     = length(var.PUBLIC_SUBNET_CIDR)
+  vpc_id                    = aws_vpc.roboshop-dev.id
+  cidr_block                = element(var.PUBLIC_SUBNET_CIDR, count.index)
+  availability_zone         = element(var.ZONE, count.index)
+  map_public_ip_on_launch   = true
   tags = {
     Name = "public_subnet-${var.ENV}-${count.index+1}"
   }
@@ -12,10 +12,10 @@ resource "aws_subnet" "public_subnet" {
 
 #Provisioning private subnet
 resource "aws_subnet" "private_subnet" {
-  count      = length(var.PRIVATE_SUBNET_CIDR)
-  vpc_id     = aws_vpc.roboshop-dev.id
-  cidr_block = element(var.PRIVATE_SUBNET_CIDR, count.index)
-  availability_zone = element(var.ZONE, count.index)
+  count                    = length(var.PRIVATE_SUBNET_CIDR)
+  vpc_id                   = aws_vpc.roboshop-dev.id
+  cidr_block               = element(var.PRIVATE_SUBNET_CIDR, count.index)
+  availability_zone        = element(var.ZONE, count.index)
 
   tags = {
     Name = "private_subnet-${var.ENV}-${count.index+1}"
