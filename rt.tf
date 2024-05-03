a3ea3bb3330e9bcb9d2a8ea06da9f35d815e2071
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-04 00:42:18 +0530

    auto push

--- rt.tf
+++ rt.tf
@@ -20,10 +20,10 @@ resource "aws_route_table_association" "public_rt_association" {
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.roboshop_dev.id
 
-#   route {
-#     cidr_block = var.VPC_CIDR
-#     gateway_id = aws_internet_gateway.ngw.id
-#   }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.ngw.id
+  }
 
   tags = {
     Name = "roboshop-${var.ENV}-private_rt"
