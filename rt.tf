7a9bc077b22930909bbaa5d20437cc0d5dfef915
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-04 13:52:21 +0530

    auto push

--- rt.tf
+++ rt.tf
@@ -44,14 +44,8 @@ resource "aws_route_table_association" "private_rt_association" {
   route_table_id    = aws_route_table.private_rt.id
 }
 
-resource "aws_default_route_table" "DEFAULT_VPC_RT" {
-  default_route_table_id = var.DEFAULT_VPC_RT
-
-  route {
-    cidr_block = var.VPC_CIDR
-    vpc_peering_connection_id = aws_vpc_peering_connection.peering_connection.id
-  }
-  tags = {
-    Name = "Default_RT_TF"
-  }
+resource "aws_route" "r" {
+  route_table_id            = var.DEFAULT_VPC_RT
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering_connection.id
 }
